a57911441593960fde1605ae39262f710cef9df9
lionel045 <ekoue7@hotmail.fr>
2024-07-09 20:45:31 +0200

    Initial Commit

---
+++ LaserGameTIgneu/LaserGameTIgneuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaserGameTIgneuApp.swift
+//  LaserGameTIgneu
+//
+//  Created by Lion on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LaserGameTIgneuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
